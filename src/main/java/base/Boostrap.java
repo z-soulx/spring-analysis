5c177bad5bea9564ee4e32009a3feda819c55976
soulx <170918810@qq.com>
2020-03-23 14:48:20 +0800

    添加案例配置

--- src/main/java/base/Boostrap.java
+++ src/main/java/base/Boostrap.java
@@ -7,9 +7,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Boostrap {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        TransactionBean bean = context.getBean(TransactionBean.class);
-        bean.process();
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+           //设置环境轮廓
+//        context.getEnvironment().setActiveProfiles("dev");
+        //测 占位符替换
+        System.setProperty("spring", "classpath");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("${spring}:config.xml");
+
+		SimpleBean bean = context.getBean(SimpleBean.class);
+        bean.send();
+        context.close();
+//		TransactionBean bean = context.getBean(TransactionBean.class);
+//        bean.process();
     }
 	
 }
